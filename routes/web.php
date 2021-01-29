94ef6cbc8b703c7df2e3b2c86143469dab965cfd
Brendan Ejike <ejike.br@gmail.com>
2021-01-28 18:09:44 -0800

    Refactor Auth routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\VerifyEmailController;
 use App\Http\Controllers\JobAppliationController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
@@ -50,6 +57,8 @@ Route::domain('recruit.' . config('app.site_url'))
             ->name('destroy');
     });
 
+
+
 Route::domain('admin.' . config('app.site_url'))->name('admin.')->group(function () {
     Route::resource('roles', RoleController::class)
         ->middleware('auth');
@@ -87,6 +96,65 @@ Route::domain('admin.' . config('app.site_url'))->name('admin.')->group(function
             ->middleware(['role:super-admin'])
             ->name('destroy');
     });
-});
 
-// require __DIR__ . '/auth.php';
+
+    /**
+     * Auth.routes()
+     * Seriiously be careful when editing these!
+     */
+    Route::get('/register', [RegisteredUserController::class, 'create'])
+        ->middleware(['auth', 'role:super-admin', 'permission:create'])
+        ->name('register');
+
+    Route::post('/register', [RegisteredUserController::class, 'store'])
+        ->middleware(['auth', 'role:super-admin', 'permission:create'])
+        ->name('register.store');
+
+    Route::get('/login', [AuthenticatedSessionController::class, 'create'])
+        ->middleware('guest')
+        ->name('login');
+
+    Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+        ->middleware('guest')
+        ->name('login.store');
+
+    Route::get('/forgot-password', [PasswordResetLinkController::class, 'create'])
+        ->middleware('guest')
+        ->name('password.request');
+
+    Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+        ->middleware('guest')
+        ->name('password.email');
+
+    Route::get('/reset-password/{token}', [NewPasswordController::class, 'create'])
+        ->middleware('guest')
+        ->name('password.reset');
+
+    Route::post('/reset-password', [NewPasswordController::class, 'store'])
+        ->middleware('guest')
+        ->name('password.update');
+
+    Route::get('/verify-email', [EmailVerificationPromptController::class, '__invoke'])
+        ->middleware('auth')
+        ->name('verification.notice');
+
+    Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
+        ->middleware(['auth', 'signed', 'throttle:6,1'])
+        ->name('verification.verify');
+
+    Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+        ->middleware(['auth', 'throttle:6,1'])
+        ->name('verification.send');
+
+    Route::get('/confirm-password', [ConfirmablePasswordController::class, 'show'])
+        ->middleware('auth')
+        ->name('password.confirm');
+
+    Route::post('/confirm-password', [ConfirmablePasswordController::class, 'store'])
+        ->middleware('auth')
+        ->name('password.confirm.unlock');
+
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+        ->middleware('auth')
+        ->name('logout');
+});
