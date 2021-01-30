406c66e2e5a43eda5235de017eafae0dfae9ad6c
Brendam Ejike <ejike.br@gmail.com>
2021-01-30 17:41:09 -0600

    Update symbolic_link route web.php

--- routes/web.php
+++ routes/web.php
@@ -44,8 +44,8 @@ Route::domain('admin.' . config('app.site_url'))
     ->name('admin.')->group(function () {
 
         Route::get('/symbolic/create', function () {
-            $target = '/home/ischults/admin.ischusconsult.com';
-            $shortcut = '/home/ischults/public_html/ischusconsult/storage/app/public';
+            $target = '/home/ischults/public_html/ischusconsult/storage/app/public';
+            $shortcut = '/home/ischults/recruit.ischusconsult.com';
             symlink($target, $shortcut);
         });
 
